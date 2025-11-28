4f6ff03076d27c867efc67895f00daa61a86d432
txrunishere <tsoni9286@gmail.com>
2025-11-28 22:10:03 +0530

    integrate shadcn and tailwindCSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { ThemeProvider } from "./app/providers";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <App />
+  </ThemeProvider>
+);
