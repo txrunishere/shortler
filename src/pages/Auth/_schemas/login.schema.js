a8c7895cea1d5ceca9d80533ff54db2f65e93539
txrunishere <tsoni9286@gmail.com>
2025-11-29 14:45:19 +0530

    create signup schema for zod validation and rewrite login schema

--- src/pages/Auth/_schemas/login.schema.js
+++ src/pages/Auth/_schemas/login.schema.js
@@ -1,11 +1,5 @@
 import { z } from "zod";
 
-// Separate regex checks
-const hasLowercase = /[a-z]/;
-const hasUppercase = /[A-Z]/;
-const hasNumber = /\d/;
-const hasSpecialChar = /[!@#$%^&*()_\-+={}[\]|\\:;"'<>,.?/~`₹]/;
-
 export const loginSchema = z.object({
   email: z
     .string({ required_error: "Email is required" })
@@ -18,18 +12,4 @@ export const loginSchema = z.object({
     .string({ required_error: "Password is required" })
     .min(8, { message: "Password must be at least 8 characters" })
     .max(128, { message: "Password must be at most 128 characters" })
-
-    // separate refinements
-    .refine((val) => hasLowercase.test(val), {
-      message: "Password must contain at least 1 lowercase letter",
-    })
-    .refine((val) => hasUppercase.test(val), {
-      message: "Password must contain at least 1 uppercase letter",
-    })
-    .refine((val) => hasNumber.test(val), {
-      message: "Password must contain at least 1 number",
-    })
-    .refine((val) => hasSpecialChar.test(val), {
-      message: "Password must contain at least 1 special character",
-    }),
 });
---
+++ src/pages/Auth/_schemas/signup.schema.js
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+export const signupSchema = z.object({
+  name: z.string().min(2, "Name must be at least 2 characters."),
+  email: z
+    .string({ required_error: "Email is required" })
+    .trim()
+    .min(5, { message: "Email is too short" })
+    .max(255, { message: "Email is too long" })
+    .email({ message: "Invalid email address" }),
+
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(8, { message: "Password must be at least 8 characters" })
+    .max(128, { message: "Password must be at most 128 characters" })
+
+    .refine((val) => hasLowercase.test(val), {
+      message: "Password must contain at least 1 lowercase letter",
+    })
+    .refine((val) => hasUppercase.test(val), {
+      message: "Password must contain at least 1 uppercase letter",
+    })
+    .refine((val) => hasNumber.test(val), {
+      message: "Password must contain at least 1 number",
+    })
+    .refine((val) => hasSpecialChar.test(val), {
+      message: "Password must contain at least 1 special character",
+    }),
+  profilePicture: z.instanceof(File, {
+    message: "Profile picture must be a valid file.",
+  }),
+});
