62a88f28ab8c7b16fe961f47ffa9b8abf4e4a90e
txrunishere <tsoni9286@gmail.com>
2025-12-01 23:04:57 +0530

    add redirect link in link card

--- src/pages/Dashboard/_components/LinkCard.jsx
+++ src/pages/Dashboard/_components/LinkCard.jsx
@@ -1,59 +1,77 @@
-import { deleteUrl } from "@/api/url.api"
-import { useUser } from "@/app/providers/user-provider"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
-import { useFetch } from "@/hooks/use-fetch"
-import config from "@/lib/config"
-import { Copy, Link, Trash } from "lucide-react"
-import QRCode from "react-qrcode-logo"
-import { BarLoader, BeatLoader } from "react-spinners"
+import { deleteUrl } from "@/api/url.api";
+import { useUser } from "@/app/providers/user-provider";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { useFetch } from "@/hooks/use-fetch";
+import config from "@/lib/config";
+import { Copy, Link, Trash } from "lucide-react";
+import QRCode from "react-qrcode-logo";
+import { BarLoader, BeatLoader } from "react-spinners";
 
 export const LinkCard = ({ url, fetchUrls }) => {
-  const { user } = useUser()
+  const { user } = useUser();
 
-  const { fn: fnDeleteUrl, loading: deleteLoading } = useFetch(deleteUrl)
+  const { fn: fnDeleteUrl, loading: deleteLoading } = useFetch(deleteUrl);
 
   const handleDeleteUrl = async () => {
-    await fnDeleteUrl({ url_id: url.id })
-    await fetchUrls({ user_id: user?.id })
-  }
+    await fnDeleteUrl({ url_id: url.id });
+    await fetchUrls({ user_id: user?.id });
+  };
 
   return (
     <>
       <div>
-        {deleteLoading && <BarLoader color="white" width={'98%'} className="mx-auto rounded-full" />}
-        <Card className="flex flex-col sm:flex-row justify-between gap-4 p-4">
-          <div className="flex flex-col sm:flex-row gap-4 w-full">
-
+        {deleteLoading && (
+          <BarLoader
+            color="white"
+            width={"98%"}
+            className="mx-auto rounded-full"
+          />
+        )}
+        <Card className="flex flex-col justify-between gap-4 p-4 sm:flex-row">
+          <div className="flex w-full flex-col gap-4 sm:flex-row">
             <div className="shrink-0">
               <QRCode value={url.original_url} />
             </div>
 
-            <CardContent className="p-0 flex flex-col gap-2 w-full">
-              <span className="font-bold text-2xl sm:text-3xl leading-snug">
+            <CardContent className="flex w-full flex-col gap-2 p-0">
+              <span className="text-2xl leading-snug font-bold sm:text-3xl">
                 {url.title}
               </span>
-              <span className="font-semibold text-xl sm:text-2xl text-blue-500 break-all">
-                {config.FRONTEND_URL}/{url.short_url ? url.short_url : url.custom_url}
+              <span className="text-xl font-semibold break-all text-blue-500 hover:underline sm:text-2xl">
+                <a
+                  target="_blank"
+                  href={url.short_url ? url.short_url : url.custom_url}
+                >
+                  {config.FRONTEND_URL}/
+                  {url.short_url ? url.short_url : url.custom_url}
+                </a>
               </span>
-              <span className="flex gap-2 items-center text-sm sm:text-lg break-all text-muted-foreground">
+              <span className="text-muted-foreground flex items-center gap-2 text-sm break-all sm:text-lg">
                 <Link size={18} />
                 {url.original_url}
               </span>
 
-              <span className="text-xs sm:text-sm text-muted-foreground">
+              <span className="text-muted-foreground text-xs sm:text-sm">
                 {new Date(url?.created_at).toLocaleString()}
               </span>
             </CardContent>
           </div>
 
-          <CardFooter className="flex gap-2 items-start justify-start sm:justify-end p-0">
+          <CardFooter className="flex items-start justify-start gap-2 p-0 sm:justify-end">
             <Button
               variant="ghost"
               size="icon"
-              onClick={() => navigator.clipboard.writeText(`
+              onClick={() =>
+                navigator.clipboard.writeText(`
             ${config.FRONTEND_URL}/${url.short_url ? url.short_url : url.custom_url}
-          `)}
+          `)
+              }
             >
               <Copy />
             </Button>
@@ -69,5 +87,5 @@ export const LinkCard = ({ url, fetchUrls }) => {
         </Card>
       </div>
     </>
-  )
-}
+  );
+};
